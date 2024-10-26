9a5063e894bf6c0e142b34b480ca72979c113eb2
Gui-rosales <guilhermelexandre456@gmail.com>
2024-10-26 12:13:41 -0400

    fix: update API base URL to use dynamic value for testing

--- frontend/src/utils/api.ts
+++ frontend/src/utils/api.ts
@@ -1,7 +1,8 @@
 import axios from 'axios';
+const apiUrl = import.meta.env.VITE_API_URL;
 
 const api = axios.create({
-  baseURL: 'https://495c-2804-26d4-201e-3400-8139-c21-76e9-9c9c.ngrok-free.app',
+  baseURL: apiUrl ?? 'http://localhost:8008',
 });
 
 //request on graph route
